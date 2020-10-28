ed6e7b8fcf8016cd51b238febc24bd0419d6a8f2
Lorena LD <38080539+lorenald@users.noreply.github.com>
2020-10-27 22:48:09 -0400

    Create calculate_age.R

---
+++ calculate_age.R
@@ -0,0 +1,21 @@
+### functions to calculate age in years, months, or days
+
+age_y <- function(dob, age.day = today(), units = "years", floor = F) {
+  calc.age = interval(dob, age.day) / duration(num = 1, units = units)
+  if (floor) return(as.integer(floor(calc.age)))
+  return(calc.age)
+}
+
+age_m <- function(dob, age.day = today(), units = "months", floor = F) {
+  calc.age = interval(dob, age.day) / duration(num = 1, units = units)
+  if (floor) return(as.integer(floor(calc.age)))
+  return(calc.age)
+}
+
+age_d <- function(dob, age.day = today(), units = "days", floor = F) {
+  calc.age = interval(dob, age.day) / duration(num = 1, units = units)
+  if (floor) return(as.integer(floor(calc.age)))
+  return(calc.age)
+}
+
+## ex. df$ageiny <- age_y(df$dob, df$date1)
